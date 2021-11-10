d6f18904a53c185dd493204b074e7cef59d0e4f5
nodirhajiev <nodir.hajiev@gmail.com>
2021-11-10 20:45:36 +0500

    master

---
+++ app.js
@@ -0,0 +1,19 @@
+Splitting()
+
+const target = document.querySelector('.text_area')
+const results = Splitting({
+  target: target,
+  by: 'chars',
+})
+
+var btn = document.querySelector('.red:last-child')
+
+btn.addEventListener('click', function () {
+  gsap.from('.char, .text_area', 1.5, {
+    duration: 1.5,
+    opacity: 1,
+    y: 500,
+    stagger: 0.09,
+    ease: 'elastic.out(0.2)',
+  })
+})
